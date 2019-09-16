4d850ed5a3026e8510b27ea355af8730d7087cbe
Joe <rice.joseph@titans.easternflorida.edu>
2019-09-15 21:56:38 -0400

    Challenge 1
            Create byte of any type
            Create short of any type
            Create int of any type
            Create long and initialize to 50000 plus 10 times the sum of the byte plus the short plus the int
    
             */

---
+++ Main.java
@@ -0,0 +1,27 @@
+/*
+Challenge 1
+
+Create byte
+Create short
+Create int
+Create long and initialize to 50000 plus 10 times the sum of the byte plus the short plus the int
+
+*/
+
+
+package com.josephrice;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte newByte = 10;
+        short newShort = 10;
+        int newInt = 10;
+
+        long newLong = 50000 + ((newByte + newShort + newInt) * 10);
+
+        System.out.println(newLong);
+
+    }
+}
